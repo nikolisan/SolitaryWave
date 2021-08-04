c3f45e9fef149f601db3f6e1972266362a3f9453
Nikolaos Andreakos <modaristis@gmail.com>
2021-08-04 20:46:12 +0100

    Finalized script to extract the maximum runup

--- get_runup.py
+++ get_runup.py
@@ -1,18 +1,36 @@
+from matplotlib.pyplot import get
 import vtk
-import paraview
+from vtk.util import numpy_support as VN
+from tqdm import tqdm
 import os
+
+
+def get_number_of_vtks(dirname):
+    count = 0
+    for filename in os.listdir(dirname):
+        if filename.startswith('PartFluid_'):
+            count += 1
+    return count
 
 def get_vtks(dirname):
     for filename in os.listdir(dirname):
         if filename.startswith('PartFluid_'):
             yield filename
 
+def get_vtks_list(dirname):
+    vlist = []
+    for v in get_vtks(dirname):
+        vlist.append(v)
+    return vlist
+
 def open_vtk(f):
     r = vtk.vtkDataSetReader()
     r.SetFileName(f)
+    r.ReadAllVectorsOn()
+    r.ReadAllScalarsOn()
     r.Update()
-    data = r.GetOutput()
-    return data
+    polydata = r.GetOutput()
+    return polydata
 
 def write_vtk(data, f):
     try:
@@ -25,12 +43,47 @@ def write_vtk(data, f):
     finally:
         return True
 
+def get_points_coords(polydata):
+    coords_array = VN.vtk_to_numpy(polydata.GetPoints().GetData())
+    return coords_array[:,0] , coords_array[:,1] , coords_array[:,2]
+
+def get_array(polydata, name):
+    return VN.vtk_to_numpy(polydata.GetPointData().GetArray(name))
+
+def get_maximum_runup(dirname):
+    max_z = []
+    nfiles = get_number_of_vtks(dirname)
+    with tqdm(total=nfiles) as pbar:
+        for f in get_vtks(dirname):
+            # print('Processing: ', f)
+            polydata = open_vtk(os.path.join(dirname, f))
+            z = get_points_coords(polydata)[2]
+            max_z.append(max(z))
+            pbar.update(1)
+
+    max_runup = max(max_z)
+    ind = max_z.index(max_runup)
+
+    return max_runup, ind
 
 if __name__ == '__main__':
-    new_dir = os.path.join(os.getcwd(),'test')
+    import matplotlib.pyplot as plt
+
+    def plot_test(dirname):
+        vlist = get_vtks_list(dirname)
+        fvtk = os.path.join(dirname, vlist[100])
+        # Get the data from the vtk file
+        polydata = open_vtk(fvtk)
+        # Get the point coordinates
+        x, y, z = get_points_coords(polydata)[0], get_points_coords(polydata)[1], get_points_coords(polydata)[2]
+        # Get the velocity in x direction
+        vel_array = get_array(polydata, 'Vel')
+        # Plot the profile
+        plt.scatter(x, z)
+        plt.show()
+
     dirname = os.path.join(os.getcwd(),'KdV\CaseSolitaryWallS2_out\particles')
-    for f in get_vtks(dirname):
-        print('Processing: ', f)
-        save_ = os.path.join(new_dir, f)
-        data = open_vtk(os.path.join(dirname, f)) 
-        write_vtk(data, save_)
+
+    max_runup, ind = get_maximum_runup(dirname)
+
+    print('Maximum runup in: ', ind, ' : ', max_runup)

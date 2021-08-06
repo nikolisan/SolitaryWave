ca39e7f2c2427dd232a10ea8ede716f7fcc373cd
Nikolaos Andreakos <modaristis@gmail.com>
2021-08-06 19:37:35 +0100

    Implemented pandas dataframes

--- get_runup.py
+++ get_runup.py
@@ -1,7 +1,7 @@
-from matplotlib.pyplot import get
-import vtk
 from vtk.util import numpy_support as VN
 from tqdm import tqdm
+import pandas as pd
+import vtk
 import os
 
 
@@ -45,45 +45,40 @@ def write_vtk(data, f):
 
 def get_points_coords(polydata):
     coords_array = VN.vtk_to_numpy(polydata.GetPoints().GetData())
-    return coords_array[:,0] , coords_array[:,1] , coords_array[:,2]
+    return coords_array
 
 def get_array(polydata, name):
     return VN.vtk_to_numpy(polydata.GetPointData().GetArray(name))
+
+def get_array_names(polydata):
+    pointdata = polydata.GetPointData()
+    return [pointdata.GetArrayName(i) for i in range(pointdata.GetNumberOfArrays())]
+
+def numpy_to_df(array, columns):
+    return pd.DataFrame(array, columns=columns)
+
+def combine_df(*dfs):
+    c = pd.concat(dfs, axis=1, join='inner')
+    return c
 
 def get_maximum_runup(dirname):
     max_z = []
     nfiles = get_number_of_vtks(dirname)
     with tqdm(total=nfiles) as pbar:
         for f in get_vtks(dirname):
-            # print('Processing: ', f)
             polydata = open_vtk(os.path.join(dirname, f))
-            z = get_points_coords(polydata)[2]
-            max_z.append(max(z))
+            coords_array = get_points_coords(polydata)
+            coords = numpy_to_df(coords_array, ['x', 'y', 'z'])
+            max_z.append(max(coords.z))
             pbar.update(1)
-
     max_runup = max(max_z)
     ind = max_z.index(max_runup)
-
     return max_runup, ind
 
+
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    import argparse
-
-    def plot_test(dirname):
-        vlist = get_vtks_list(dirname)
-        fvtk = os.path.join(dirname, vlist[100])
-        # Get the data from the vtk file
-        polydata = open_vtk(fvtk)
-        # Get the point coordinates
-        x, y, z = get_points_coords(polydata)[0], get_points_coords(polydata)[1], get_points_coords(polydata)[2]
-        # Get the velocity in x direction
-        vel_array = get_array(polydata, 'Vel')
-        # Plot the profile
-        plt.scatter(x, z)
-        plt.show()
-
-
+    import argparse    
+   
     parser = argparse.ArgumentParser(description='Extracts the maximum runup value for a given simulation')
     parser.add_argument('-c', '--case', dest='case', required=True,metavar='CASE', help='Case name')
     parser.add_argument('-m', '--method', dest='method', required=False, metavar='METHOD', help='Wave generation method')
